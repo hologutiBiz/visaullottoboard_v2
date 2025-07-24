4e7a8fd54dcd70f91fe18e2f077333f443473d90
hologutbiz <holoakinseloyin@gmail.com>
2025-07-24 16:16:43 +0100

    integrate QRcode for app

---
+++ script/qrCode.js
@@ -0,0 +1,9 @@
+// Lotto Classification Chart
+var counterpartQrCode = new QRCode("counterpart_qrCode", {
+    text: "https://play.google.com/store/apps/details?id=com.visuallottoboard.lottoclassificationchart",
+    // width: 300,
+    // height: 350,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+});

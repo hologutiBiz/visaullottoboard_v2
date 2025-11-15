8746d8e3ab9482b81a108d79a804936446bafb05
hologutbiz <holoakinseloyin@gmail.com>
2025-11-15 11:02:09 +0100

    added baba-ijebu-app to download page

--- script/qrCode.js
+++ script/qrCode.js
@@ -5,3 +5,12 @@ var counterpartQrCode = new QRCode("counterpart_qrCode", {
     colorLight: "#ffffff",
     correctLevel : QRCode.CorrectLevel.H
 });
+
+
+// Baba Ijebu Lotto Results
+var counterpartQrCode = new QRCode("babaIjebuResultApp_qrCode", {
+    text: "https://play.google.com/store/apps/details?id=com.visuallottoboard.",
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+});
